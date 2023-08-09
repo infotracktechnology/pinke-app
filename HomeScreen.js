c74cc5f633ecdd1cba6750a307a24ca1401e6133
infotracktechnology <infotracktechnologysolutions@gmail.com>
2023-08-09 14:46:37 +0530

    ok

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,13 +1,53 @@
+import React, { useReducer } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Stack, ListItem, Avatar } from '@react-native-material/core'; // Corrected import
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
-import React from 'react';
-import { View, Text } from 'react-native';
+const initialContacts = [
+  {
+    id: 1,
+    name: 'John Doe',
+    phone: '123-456-7890',
+    photoUrl: 'https://mui.com/static/images/avatar/1.jpg', 
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    phone: '987-654-3210',
+    photoUrl: 'https://mui.com/static/images/avatar/1.jpg', 
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+  
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
+  const [contacts, dispatch] = useReducer(reducer, initialContacts);
+
+  const handleCall = (phone) => {
+
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <Stack spacing={4} mt={10}>
+
+      {contacts.map((contact) => (
+        <ListItem
+          key={contact.id}
+          title={contact.name}
+          overline={contact.phone}
+          meta={<MaterialCommunityIcons name="phone"  color='black' size={20}/>}
+          onPress={() => {}}
+        />
+      ))}
+    </Stack>
   );
 }
 
 export default HomeScreen;
+
--- NavigationContainer.js
+++ NavigationContainer.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 
 import HomeScreen from './HomeScreen'; 
@@ -12,7 +13,23 @@ const Tab = createBottomTabNavigator();
 function NavigationContainerComponent() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator  
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Setting') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Setting" component={Setting} />
       </Tab.Navigator>
